274bbd1c0f454a05a3bbdf45103a7f7cb4c58cf9
Abhishek Anand <mailtomeabhi15@gmail.com>
2025-10-16 20:51:06 +0530

    Setup and add a test route

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const apiRoutes = require('./routes/index');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api',apiRoutes);
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/creditsea';
